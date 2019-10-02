9adece6185815e06aaea8fabf46059acb2b8d9ad
Parag <paragspatil123@gmail.com>
2019-10-02 09:06:06 +0530

    day 3 student exercise

--- bin/main.dart
+++ bin/main.dart
@@ -1,11 +1,40 @@
-
+import 'dart:io';
 void main(){
   List<String> names = ["Kimiko", "Raj", "Anu", "Paul"];
+  bool func(List j){
+      bool flag = true;
+      while(flag){
+        print("a(add) d(delete),e(end)");
+        print("enter yor choice");
+
+        String z = stdin.readLineSync();
+        if (z == 'a') {
+          print('enter name of student to add');
+          String x = stdin.readLineSync();
+          names.add(x);
+          print(names);
+        }
+        else if (z == 'd'){
+          print('enter name of stident to delete');
+        String v = stdin.readLineSync();
+        names.remove(v);
+        print(names);
+      }
+     else if(z == 'e'){
+       flag == false;
+
+
+        }
+        }
+      }
+      func(names);
+
+  }
 
 // Write a program to allow user to modify the given list as shown below.
 // Add Student, remove student and stop the program
 
-}
+
 
 //[Kimiko, Raj, Anu, Paul]
 //(a)dd or (r)emove a student, or (e)xit?
